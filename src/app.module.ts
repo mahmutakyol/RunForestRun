6a9d61c7192b0ffacad70ffe81d904b9a2294059
Mahmut Akyol <mahmutakyol@192.168.1.7>
2022-03-23 20:59:13 +0300

    Remove unnecessary files and add device entity properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { TestRunsModule } from './test-runs/test-runs.module';
 import { ScenariosModule } from './scenarios/scenarios.module';
 import { DevicesModule } from './devices/devices.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TestRunsModule, ScenariosModule, DevicesModule],
+  imports: [
+    // MONGOPASS=QW7wYwtTLzCcfyJh
+    MongooseModule.forRoot(
+      'mongodb+srv://mahmutakyol:QW7wYwtTLzCcfyJh@cluster0.triyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+    TestRunsModule,
+    ScenariosModule,
+    DevicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/devices/entities/device.entity.ts
+++ src/devices/entities/device.entity.ts
@@ -1,1 +1,21 @@
-export class Device {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type DeviceDocument = Device & Document;
+
+@Schema()
+export class Device {
+  @Prop()
+  name: string;
+
+  @Prop()
+  manufacturer: string;
+
+  @Prop()
+  model: string;
+
+  @Prop()
+  os: string;
+}
+
+export const DeviceSchema = SchemaFactory.createForClass(Device);
