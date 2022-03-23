834f07d556e3cf566e839820312ae64752881112
Mahmut Akyol <mahmutakyol@192.168.1.7>
2022-03-24 00:13:02 +0300

    add status codes

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { CreateDeviceDto } from './dto/create-device.dto';
@@ -17,21 +18,25 @@ export class DevicesController {
   constructor(private readonly devicesService: DevicesService) {}
 
   @Post()
+  @HttpCode(201)
   async create(@Body() createDeviceDto: CreateDeviceDto) {
     return this.devicesService.create(createDeviceDto);
   }
 
   @Get()
+  @HttpCode(200)
   async findAll() {
     return this.devicesService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(200)
   async findOne(@Param('id') id: string) {
     return this.devicesService.findOne(id);
   }
 
   @Patch(':id')
+  @HttpCode(202)
   async update(
     @Param('id') id: string,
     @Body() updateDeviceDto: UpdateDeviceDto,
@@ -40,6 +45,7 @@ export class DevicesController {
   }
 
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.devicesService.remove(+id);
   }
